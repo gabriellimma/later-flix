67a636b9232c5ca09b405a4ea5e41f0f68a465b0
Bielzin42 <nickspoot@gmail.com>
2020-07-28 11:41:20 -0300

    creating routes and cadastroVideo component :sparkles:

--- src/index.js
+++ src/index.js
@@ -2,12 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CadastroVideo from './components/CadastroVideo/cadastro';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route component={() => (<h1>404 Página não encontrada </h1>)} />
+   </Switch>    
+  </BrowserRouter>,
+ 
   document.getElementById('root')
 );
 
